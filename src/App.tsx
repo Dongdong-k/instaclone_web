4372b579bd3c74bfee5fe681d9f34b38ea4d7079
dongdong <crisish@naver.com>
2021-06-24 16:18:52 +0900

    #8.1 Installing All (06:08)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { ourTheme } from "./styles";
+
+const Container = styled.div`
+  background-color: ${(prop) => prop.theme.bgColor};
+  color: ${(prop) => prop.theme.fontColor};
+`;
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={ourTheme}>
+      <Container>Empty</Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
