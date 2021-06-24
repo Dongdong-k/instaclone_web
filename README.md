5dff4ac9441234e85d65203b53fb027d44765cb2
dongdong <crisish@naver.com>
2021-06-24 17:26:15 +0900

    #8.4 Router Setup part Two (10:22)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Instaclone Web
+
+## Setup Meaning
+
+- [x] Router
+- [ ] Authentication
+- [ ] Arch. - file/component tree
+- [ ] styles - font, color, dark mode etc...
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 // import styled, { ThemeProvider } from "styled-components";
 // import { ourTheme } from "./styles";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 // const Container = styled.div`
 //   background-color: ${(prop) => prop.theme.bgColor};
@@ -8,6 +16,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 // `;
 
 function App() {
+  const isloggedIn = true;
   return (
     <div>
       {/* 라우터 설정 */}
@@ -18,13 +27,14 @@ function App() {
         <Switch>
           {/* exact : path가 완전히 동일할 경우에만 실행 */}
           <Route exact path="/">
-            <h1>Home</h1>
+            {isloggedIn ? <Home /> : <Login />}
           </Route>
-          <Route exact path="/Potato">
-            <h1>Potato</h1>
-          </Route>
-          <Route exact path="/Banana">
-            <h1>Banana</h1>
+          {/* 없는 페이지 주소 입력시 아래와 같이 출력, 마지막에 입력하기 */}
+          <Route>
+            {/* 1안) 404 Not Found 페이지 연결하기 */}
+            <NotFound />
+            {/* 2안) 다른 페이지 주소로 연결하기 */}
+            {/* <Redirect to="/" />  */}
           </Route>
         </Switch>
       </Router>
