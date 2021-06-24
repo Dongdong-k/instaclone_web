3d9bf80a508ef3f96c372897360684a80f9bc281
dongdong <crisish@naver.com>
2021-06-24 16:59:31 +0900

    #8.3 Router Setup part One (10:29

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import styled, { ThemeProvider } from "styled-components";
-import { ourTheme } from "./styles";
+// import styled, { ThemeProvider } from "styled-components";
+// import { ourTheme } from "./styles";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-const Container = styled.div`
-  background-color: ${(prop) => prop.theme.bgColor};
-  color: ${(prop) => prop.theme.fontColor};
-`;
+// const Container = styled.div`
+//   background-color: ${(prop) => prop.theme.bgColor};
+//   color: ${(prop) => prop.theme.fontColor};
+// `;
 
 function App() {
   return (
-    <ThemeProvider theme={ourTheme}>
-      <Container>Empty</Container>
-    </ThemeProvider>
+    <div>
+      {/* 라우터 설정 */}
+      <Router>
+        {/* Switch : 하나의 라우터만 rendering */}
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          {/* exact : path가 완전히 동일할 경우에만 실행 */}
+          <Route exact path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route exact path="/Potato">
+            <h1>Potato</h1>
+          </Route>
+          <Route exact path="/Banana">
+            <h1>Banana</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
