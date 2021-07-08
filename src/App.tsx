3bf80ffb4553a65cc0b5e6188ac65c20fbdaa3d0
dongdong <crisish@naver.com>
2021-07-08 18:04:49 +0900

    #10.10 Login part One (09:26)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // import styled, { ThemeProvider } from "styled-components";
 // import { ourTheme } from "./styles";
-import { useReactiveVar } from "@apollo/client";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
@@ -30,36 +30,38 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalTheme />
-      {/* <Container floating={true}> */}
-      {/* 라우터 설정 */}
-      <Router>
-        {/* Switch : 하나의 라우터만 rendering */}
-        {/* A <Switch> looks through its children <Route>s and
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <GlobalTheme />
+        {/* <Container floating={true}> */}
+        {/* 라우터 설정 */}
+        <Router>
+          {/* Switch : 하나의 라우터만 rendering */}
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          {/* exact : path가 완전히 동일할 경우에만 실행 */}
-          <Route exact path={routes.home}>
-            {isLoggedIn ? <Home /> : <Login />}
-          </Route>
-          {/* 로그인 된 경우에는 가입 페이지 안보이게 하기 */}
-          {!isLoggedIn ? (
-            <Route exact path={routes.signUp}>
-              <SignUp />
+          <Switch>
+            {/* exact : path가 완전히 동일할 경우에만 실행 */}
+            <Route exact path={routes.home}>
+              {isLoggedIn ? <Home /> : <Login />}
             </Route>
-          ) : null}
-          {/* 없는 페이지 주소 입력시 아래와 같이 출력, 마지막에 입력하기 */}
-          <Route>
-            {/* 1안) 404 Not Found 페이지 연결하기 */}
-            <NotFound />
-            {/* 2안) 다른 페이지 주소로 연결하기 */}
-            {/* <Redirect to="/" />  */}
-          </Route>
-        </Switch>
-      </Router>
-      {/* </Container> */}
-    </ThemeProvider>
+            {/* 로그인 된 경우에는 가입 페이지 안보이게 하기 */}
+            {!isLoggedIn ? (
+              <Route exact path={routes.signUp}>
+                <SignUp />
+              </Route>
+            ) : null}
+            {/* 없는 페이지 주소 입력시 아래와 같이 출력, 마지막에 입력하기 */}
+            <Route>
+              {/* 1안) 404 Not Found 페이지 연결하기 */}
+              <NotFound />
+              {/* 2안) 다른 페이지 주소로 연결하기 */}
+              {/* <Redirect to="/" />  */}
+            </Route>
+          </Switch>
+        </Router>
+        {/* </Container> */}
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,4 +1,12 @@
-import { makeVar } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
 export const isLoggedInVar = makeVar(false);
 export const darkModeVar = makeVar(false);
+
+// 백엔드와 프런트엔드 연결하기
+export const client = new ApolloClient({
+  // uri : GraphQl application 위치 알려주는 기능
+  uri: "http://localhost:4000/graphql",
+  // Cache : Apollo에서 가져온 정보를 기억
+  cache: new InMemoryCache(),
+});
