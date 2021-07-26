7298c099d9ef385f3b670906239fb3c010425574
dongdong <crisish@naver.com>
2021-07-26 14:45:00 +0900

    #11.4 Photo Component part One (10:32)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,59 @@
+import { gql, useQuery } from "@apollo/client";
 import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 import { logUserOut } from "../apollo";
+import Avatar from "../components/Avatar";
 import PageTitle from "../components/PageTitle";
+import { FatText } from "../components/shared";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        userName
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  margin-bottom: 20px;
+  border: 1px solid ${(props) => props.theme.borderColor};
+`;
+const PhotoHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 10px;
+`;
 
 const Home = () => {
+  const { data } = useQuery(FEED_QUERY);
+  console.log(data);
   const history = useHistory(); // SignUp 에서 전달받은 history.locaiton.state 초기화를 위해 설정
   // button click 시 logUserOut() 실행할때 history.replace 실행을 위해 argu 로 전달
-  console.log("Home history", history);
   return (
     <div>
       <PageTitle title="Home" />
-      <h1>Welcome, We did it!!</h1>
+      {data?.seeFeed?.map((photo: any) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.userName}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
       <button onClick={() => logUserOut(history)}>Log out now!</button>
     </div>
   );
